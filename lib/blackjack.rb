55a33147964818313414f321074ccb8d1ebe97c7
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 02:50:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,8 +56,8 @@ welcome
 card_total=initial_round
 prompt_user
 until card_total > 21
-card_total = hit?(card_total
-display_card_total(card_total))
+card_total = hit?(card_total)
+display_card_total(card_total)
 end
 end_game(card_total)
 end

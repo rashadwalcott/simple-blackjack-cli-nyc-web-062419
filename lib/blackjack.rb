f890925c7788a39cf4bdaacda15a4ffdbb9a8db1
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 01:34:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,15 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  num = gets.chomp.to_i
+  card_total = num
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user

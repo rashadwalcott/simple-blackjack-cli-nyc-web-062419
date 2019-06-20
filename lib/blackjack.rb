b974c853381d9e0a32b3bd30d46eb80d9f2dcf0c
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 02:50:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,9 +53,12 @@ end
 
 def runner
 welcome
-initial_round
+card_total=initial_round
 prompt_user
-hit?
-
+until card_total > 21
+card_total = hit?(card_total
+display_card_total(card_total))
+end
+end_game(card_total)
 end
     
